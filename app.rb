6473338bd98cf0efabfee905f7ea84fa9c11d01d
Tuyet Le <toowee.le@gmail.com>
2021-01-08 11:26:56 +0000

    Refactor POST request to use a redirect loop with session variables set

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, "My session secret"
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     p params
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect(:play)
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
