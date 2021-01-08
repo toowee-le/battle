b56b965e2821dfea58c773b037c1f9e62d109bf1
Tuyet Le <toowee.le@gmail.com>
2021-01-08 14:29:42 +0000

    Add a helper method to DRY up the feature test code

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,6 @@
 feature "Enter names" do
   scenario "players can submit their names" do
-    visit("/")
-
-    fill_in(:player_1, with: "Joe")
-    fill_in(:player_2, with: "Jamie")
-
-    click_button "Start"
-
+    sign_in_and_play
     expect(page).to have_content('Joe vs. Jamie')
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,12 +1,6 @@
 feature "View hit points" do
   scenario "view player 2's hit points" do
-    visit("/")
-
-    fill_in(:player_1, with: "Joe")
-    fill_in(:player_2, with: "Jamie")
-
-    click_button "Start"
-
+    sign_in_and_play
     expect(page).to have_content('Jamie: 60 HP')
   end
 end
