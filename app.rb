fafd278d3188d2ea36fadcddae51129dd066cd8a
Tuyet Le <toowee.le@gmail.com>
2021-01-11 10:39:36 +0000

    User story 3: pass feature test to  show confirmation that player 2 was attacked

--- app.rb
+++ app.rb
@@ -21,6 +21,17 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
+  get '/attack' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:attack)
+  end
+
+  post '/attack' do
+    p params
+    redirect(:attack)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
